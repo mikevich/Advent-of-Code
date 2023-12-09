c02b181688648de535f43fea7e28f2c7b0c8e380
Mike <vichnitchkine@gmail.com>
2023-12-09 17:01:14 -0500

    Advent of Code - Day 1

---
+++ Day 1/calibrator.py
@@ -0,0 +1,17 @@
+import re
+
+with open('input.txt') as input:
+    lines = input.read().splitlines()
+    output_nums = []
+    for line in lines:
+        num_strings = re.findall(r'\d', line)
+        nums = []
+        for string in num_strings:
+            nums.append(int(string))
+        output_nums.append(nums[0] * 10 + nums[-1])
+    
+    total = 0
+    for num in output_nums:
+        total += num
+    
+    print(total)
